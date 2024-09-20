e0ed1f84ce367fe812aab18b9f874e710f971c10
andrebariani <andrebariani@gmail.com>
2024-09-20 10:11:55 -0300

    fish detects tackle more smoothly

--- scenes/entities/shadow/shadow.gd
+++ scenes/entities/shadow/shadow.gd
@@ -4,9 +4,11 @@ class_name Shadow
 @export var fish: FishData
 
 @onready var sprite = $Line2D
-@onready var roamTimer = $RoamingTimer
 @onready var detectArea = $DetectArea
 @onready var lineOfSight = $LineOfSight
+
+@onready var roamTimer = $RoamingTimer
+@onready var chaseTimer = $ChaseTimer
 
 const SPEED = 50.0
 const ROTATION_SPEED = 1.2
@@ -59,15 +61,16 @@ func _physics_process(delta):
 			rotation = lerp_angle(rotation, dir.normalized().angle(), 0.05)
 			
 			if global_position.distance_to(target_pos) <= 1:
-				print_debug('idle')
 				roamTimer.start(randi_range(1, 5))
 				state = IDLE
 		CHASE:
 			var bait = detectArea.bait
 			if bait != null:
 				var dir = global_position.direction_to(bait.global_position)
-				velocity = velocity.move_toward(dir * SPEED, ACCEL * delta)
-				look_at(bait.global_position)
+				rotation = lerp_angle(rotation, dir.normalized().angle(), 0.05)
+				
+				if chaseTimer.is_stopped():
+					velocity = velocity.move_toward(dir * SPEED, ACCEL * delta)
 			else:
 				state = IDLE
 				
@@ -78,8 +81,9 @@ func _physics_process(delta):
 	
 func seek_bait():
 	if detectArea.can_see_bait():
-		print_debug('chase!')
 		state = CHASE
+		chaseTimer.start(randi_range(0.5, 2))
+		
 
 
 func update_target_position():
@@ -91,4 +95,7 @@ func update_target_position():
 
 func _on_roaming_timer_timeout():
 	update_target_position()
-	print_debug('go somewhere', target_pos)
+
+
+func _on_chase_timer_timeout():
+	pass
