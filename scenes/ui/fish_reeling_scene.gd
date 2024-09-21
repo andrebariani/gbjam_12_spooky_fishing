e21a452b1fae19a7748573ff618ca23c59ca2cc8
andrebariani <andrebariani@gmail.com>
2024-09-21 11:09:33 -0300

    extra anim to reeling

--- scenes/ui/fish_reeling_scene.gd
+++ scenes/ui/fish_reeling_scene.gd
@@ -5,6 +5,8 @@ class_name FishReelingScene
 @export var input_reel := 'A'
 @export var input_tug := 'B'
 
+@onready var sprite = $Fish
+@onready var hook_sprite = $Hook
 @onready var flailTimer = $FlailTimer
 @onready var struggleTimer = $StruggleTimer
 @onready var bg = $ParallaxBackground
@@ -12,6 +14,8 @@ class_name FishReelingScene
 @onready var lineTensionLabel = $LineTension
 @onready var distanceLabel = $Distance
 @onready var staminaLabel = $Stamina
+
+@onready var tween: Tween
 
 @export var fish: FishData
 
@@ -31,6 +35,7 @@ var inputs = {
 }
 
 enum {
+	WAIT,
 	REST,
 	FLAIL,
 	STRUGGLE,
@@ -56,20 +61,28 @@ func init(_fish):
 	max_stamina = 100
 	stamina = max_stamina
 
+var button_pressed = false
+var tug_just_pressed = false
 
 func _physics_process(delta):
 	update_inputs()
 	
-	var button_pressed = get_input(input_reel, 'pressed')
-	var tug_just_pressed = get_input(input_tug, 'just_pressed')
+	button_pressed = get_input(input_reel, 'pressed')
+	tug_just_pressed = get_input(input_tug, 'just_pressed')
 	
 	match state:
+		WAIT:
+			return
 		REST:
 			if button_pressed:
-				distance += distance_rate * delta
+				add_distance(distance_rate, delta)
+				add_line_tension(line_tension_rate * 0.4, delta)
+			else:
+				add_distance(-distance_rate * 0.5, delta)
+				add_line_tension(-line_tension_rate * 4, delta)
 			if tug_just_pressed:
-				distance += (distance_rate * 10) * delta
-				line_tension += line_tension_rate * 10 * delta
+				add_distance(distance_rate * 10, delta)
+				add_line_tension(line_tension_rate * 10, delta)
 			
 			add_stamina(stamina_rate, delta)
 			
@@ -82,10 +95,11 @@ func _physics_process(delta):
 					struggleTimer.start(sec + randf_range(0, 1))
 		FLAIL:
 			if button_pressed:
-				distance += (distance_rate / 2.0) * delta
-				line_tension += line_tension_rate * delta
+				add_distance(distance_rate / 2.0, delta)
+				add_line_tension(line_tension_rate, delta)
 			else:
-				distance -= (distance_rate * 0.5) * delta
+				add_distance(-distance_rate * 0.75, delta)
+				add_line_tension(-line_tension_rate, delta)
 				
 			add_stamina(-stamina_rate * 0.7, delta)
 			
@@ -93,10 +107,11 @@ func _physics_process(delta):
 				state = REST
 		STRUGGLE:
 			if button_pressed:
-				distance += (distance_rate / 5.0) * delta
-				line_tension += (line_tension_rate * 4) * delta
+				add_distance(-distance_rate / 5.0, delta)
+				add_line_tension(line_tension_rate * 4, delta)
 			else:
-				distance -= (distance_rate) * delta
+				add_distance(-distance_rate, delta)
+				add_line_tension(-line_tension_rate * 0.5, delta)
 				
 			add_stamina(-stamina_rate * 1.5, delta)
 			
@@ -111,13 +126,73 @@ func _physics_process(delta):
 	$state.text = str(state)
 	
 	bg.scroll_offset.x = -distance * 10
+	bg.scroll_offset.y = distance / 10.0
+	#bg.scroll_offset.y = -bg.scroll_offset.y
+	
+	animate_body(delta)
 	
 	if line_tension >= 100 or distance <= 0:
 		print_debug('It got away...')
 		state = RESULT
+		play_got_away_anim()
+		flailTimer.stop()
+		struggleTimer.stop()
 	elif distance >= 100:
 		print_debug('You got a ', fish.name)
 		state = RESULT
+		flailTimer.stop()
+		struggleTimer.stop()
+		
+
+var sprite_flip = false
+func animate_body(_delta):
+	if state != RESULT:
+		hook_sprite.visible = !hook_sprite.visible
+	else:
+		hook_sprite.visible = true
+	if state == REST:
+		if sprite_flip != button_pressed:
+			sprite_flip = button_pressed
+			flip_fish()
+	else:
+		if sprite_flip:
+			sprite_flip = false
+			flip_fish()
+			
+
+func flip_fish():
+	tween = get_tree().create_tween()
+	tween.stop()
+	if sprite_flip:
+		tween.tween_property(sprite, 'scale', Vector2(-1, 1), 0.3) \
+			.set_ease(Tween.EASE_IN) \
+			.set_trans(Tween.TRANS_QUAD)
+		tween.set_parallel()
+		tween.tween_property(hook_sprite, 'position', Vector2(206, 0), 0.3) \
+			.set_ease(Tween.EASE_IN) \
+			.set_trans(Tween.TRANS_QUAD)
+	else:
+		tween.tween_property(sprite, 'scale', Vector2(1, 1), 0.3) \
+			.set_ease(Tween.EASE_IN) \
+			.set_trans(Tween.TRANS_QUAD)
+		tween.set_parallel()
+		tween.tween_property(hook_sprite, 'position', Vector2(123, 0), 0.3) \
+			.set_ease(Tween.EASE_IN) \
+			.set_trans(Tween.TRANS_QUAD)
+	tween.play()
+	
+	
+func play_got_away_anim():
+	hook_sprite.visible = true
+	hook_sprite.frame = 1
+	tween = get_tree().create_tween()
+	tween.stop()
+	tween.tween_property(sprite, 'position', Vector2(-50, 80), 1.5) \
+		.set_ease(Tween.EASE_IN) \
+		.set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(hook_sprite, 'position', Vector2(123, 20), 10) \
+		.set_trans(Tween.TRANS_LINEAR)
+	tween.play()
 
 
 func _on_timer_timeout():
@@ -129,9 +204,18 @@ func _on_flail_timer_timeout():
 	struggleTimer.start(randf_range(0.5, 5))
 
 
+func add_distance(rate, delta):
+	distance += rate * delta
+	distance = clamp(distance, 0, 100)
+
 func add_stamina(rate, delta):
-	stamina += rate * 10 * delta
+	stamina += rate * 8 * delta
 	stamina = clamp(stamina, 0, max_stamina)
+
+
+func add_line_tension(rate, delta):
+	line_tension += rate * delta
+	line_tension = clamp(line_tension, 0, 100)
 
 
 func update_inputs():
